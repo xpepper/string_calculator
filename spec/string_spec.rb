5c7368f2ba98b49659f46da57bcc3898f8261e04
Pietro Di Bello <pietro.dibello@xpeppers.com>
2012-10-22 22:28:10 +0200

    Numbers bigger than 1000 should be ignored, so adding 2 + 1001  = 2

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -2,17 +2,17 @@ require_relative '../string'
 
 describe String do
   describe "#addition" do
-    
+
     context "empty string" do
       it { "".addition.should == 0 }
     end
-    
+
     context "string with one number only" do
       it "just gives you that number" do
         "1".addition.should == 1
       end
     end
-    
+
     context "string with two comma-separated numbers" do
       it "is the sum of the two numbers" do
         "1,2".addition.should == 3
@@ -30,24 +30,26 @@ describe String do
         "1\n2,3".addition.should == 1+2+3
       end
     end
-    
+
     context "support different custom delimiters" do
       # “//[delimiter]\n[numbers…]”
-      it "does something" do
-        "//;\n3;5".addition.should == 8
-      end
+      it { "//;\n3;5".addition.should == 8 }
     end
-    
+
     context "negative numbers" do
       it "throws a 'negatives not allowed' exception" do
-        # Calling Add with a negative number will throw an exception "negatives not allowed" - 
+        # Calling Add with a negative number will throw an exception "negatives not allowed" -
         # and the negative that was passed.
         # if there are multiple negatives, show all of them in the exception message
         expect { "1,-2".addition       }.to raise_error("negatives not allowed: -2")
         expect { "1,-2,-3,-4".addition }.to raise_error("negatives not allowed: -2,-3,-4")
       end
     end
-    
+
+    context "ignore numbers bigger than 1000" do
+      it { "1,1000,1001".addition.should == 1001 }
+    end
+
   end
-  
+
 end
--- string.rb
+++ string.rb
@@ -2,19 +2,28 @@ class String
   def addition
     delimiter = ","
     string_of_numbers = self
-    
+
     if start_with? "//"
       delimiter = self[2,1]
       string_of_numbers = self[4..-1]
     end
-    
+
     numbers = string_of_numbers.sub("\n", delimiter).split(delimiter).as_numbers
-    if numbers.any? {|number| number < 0}
-      negatives = numbers.select {|number| number < 0}
+    numbers.reject! {|number| number > 1000}
+
+    if numbers.any?(&:negative?)
+      negatives = numbers.select(&:negative?)
       raise Exception.new("negatives not allowed: #{negatives.join(',')}")
     else
       numbers.sum
     end
+  end
+
+end
+
+class Fixnum
+  def negative?
+    self < 0
   end
 end
 
@@ -22,7 +31,7 @@ class Array
   def sum
     inject(0, :+)
   end
-  
+
   def as_numbers
     map(&:to_i)
   end
